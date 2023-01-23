c50af47c46f5afad08cfc11ad4341c6cafe2dd85
Vanessa <vanessa@Vanessas-MacBook-Pro.local>
2023-01-23 11:50:02 -0600

    updated input notes and variable names

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 | Big-O          | O(log n)                                           |
 | Data Structure | Array                                              |
 | Class          | Search algorithm                                   |
-| Input          | Sorted Array                                       |
+| Input          | Sorted Array, itemLookingFor                       |
 | Returns        | Index of Element in the Array<br>or returns `null` |
 | Types          | Iterative<br>Recursive                             |
--- binarySearch.js
+++ binarySearch.js
@@ -1,16 +1,16 @@
-function binarySearch(list, item) {
+function binarySearch(array, itemLookingFor) {
   let lowestIndex = 0;
-  let highestIndex = length(list);
+  let highestIndex = length(array);
 
   while (lowestIndex <= highestIndex) {
     let middleIndex = lowestIndex + highestIndex / 2;
-    let guess = list[middleIndex];
+    let guess = array[middleIndex];
 
-    if (guess === item) {
+    if (guess === itemLookingFor) {
       return middleIndex;
     }
 
-    if (guess > item) {
+    if (guess > itemLookingFor) {
       highestIndex = middleIndex - 1;
     } else {
       lowestIndex = middleIndex + 1;
@@ -18,5 +18,4 @@ function binarySearch(list, item) {
 
     return null;
   }
-
 }
